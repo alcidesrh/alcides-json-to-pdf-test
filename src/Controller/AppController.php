075a478f2ede85b76ecf842dc465d8938b2d5875
alcidesrh <alcidesrh@gmail.com>
2022-08-01 20:44:03 -0600

    Adding docker.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,14 +5,47 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Snappy\Pdf;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 
 class AppController extends AbstractController
 {
+
     #[Route('/', name: 'app_app')]
-    public function index(): Response
+    public function index(Pdf $knpSnappyPdf): Response
     {
-        return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
-        ]);
+
+        if (isset($_POST["submit"])) {
+
+            $file = $_FILES["fileJson"]["tmp_name"];
+            $strJsonFileContents = file_get_contents($file);
+            $array = json_decode($strJsonFileContents, true)['Blocks'];
+
+            $data = array_map(
+                fn ($value) => $value['Text'],
+                array_values(
+                    array_filter(
+                        $array,
+                        fn ($value) => isset($value['BlockType']) && $value['BlockType'] == 'LINE'
+                    )
+                )
+            );
+
+            // return $this->render('app/pdf.html.twig', ['data' => $data]);
+
+            $html = $this->renderView(
+                'app/pdf.html.twig',
+                [
+                    'data' => $data,
+                ]
+            );
+
+            return new PdfResponse(
+                $knpSnappyPdf->getOutputFromHtml($html),
+                'file.pdf'
+            );
+        }
+
+        return $this->render('app/index.html.twig');
     }
 }
